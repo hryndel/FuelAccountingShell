ada7c0d9817c92c6bf589580e2dced010a0f2796
hryndel <andramena2004@gmail.com>
2024-05-24 21:41:21 +0300

    Изменил токен

--- FuelAccountingShell/Infrastructure/JSonWebToken.cs
+++ FuelAccountingShell/Infrastructure/JSonWebToken.cs
@@ -2,6 +2,6 @@
 {
     public static class JSonWebToken
     {
-        public static string Token { get; set; } = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiVGVzdCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFkbWluaXN0cmF0b3IiLCJleHAiOjE3MTY1OTk3ODAsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjU1NTUvIiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NTU1NS8ifQ.wu84Y6xY_7-mU6Nn2ay8V6AIjUUNdxZnkirnf5lLoOQ";
+        public static string Token { get; set; }
     }
 }
